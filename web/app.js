6517246a51034830c5ec34b0b5944213bb9be2b6
Jarrett-Long <jarrett.a.long@gmail.com>
2018-08-30 10:22:08 -0500

    tracking performance tweaks

--- web/app.js
+++ web/app.js
@@ -6,26 +6,15 @@
     var videoSnapshot = document.getElementById('video-snapshot');
     var videoSnapshotContext = videoSnapshot.getContext('2d');
 
-    var aspectRatio = (1 + (1 / 3));
-    setScreenSize(liveVideo, videoOverlay, videoSnapshot, window.innerHeight, aspectRatio);
-    window.onresize = function () {
-        setScreenSize(liveVideo, videoOverlay, videoSnapshot, window.innerHeight, aspectRatio);
-    }
-
-    var btn = document.getElementById("btn-fullscreen");
-    btn.onclick = function () {
-        liveVideo.webkitRequestFullScreen();
-    }
-
     var tracker = new tracking.ObjectTracker('face');
     tracker.setInitialScale(4);
-    tracker.setStepSize(2);
+    tracker.setStepSize(0.5);
     tracker.setEdgesDensity(0.1);
-
-    tracking.track('#live-video', tracker, { camera: true });
-
-    var trackerEnabled = true;
     tracker.on('track', handleTrackerEvent);
+    var trackerEnabled = true;
+   
+    var trackerTask = tracking.track(liveVideo, tracker, { camera: true });
+    trackerTask.run();
 
     function handleTrackerEvent(event) {
         if (!trackerEnabled) return;
@@ -73,6 +62,20 @@
 
         // log from top down
         consoleWindow.innerHTML = new Date().getTime() + ' - ' + message + '<br />' + consoleWindow.innerHTML;
+    }
+
+    var aspectRatio = (1 + (1 / 3));
+    setScreenSize(liveVideo, videoOverlay, videoSnapshot, window.innerHeight, aspectRatio);
+    window.onresize = function () {
+        trackerTask.stop();
+        setScreenSize(liveVideo, videoOverlay, videoSnapshot, window.innerHeight, aspectRatio);
+        videoOverlayContext = videoOverlay.getContext('2d');
+        trackerTask = tracking.track(liveVideo, tracker);
+    }
+
+    var btn = document.getElementById("btn-fullscreen");
+    btn.onclick = function () {
+        liveVideo.webkitRequestFullScreen();
     }
 
     function setScreenSize(liveVideo, videoOverlay, videoSnapshot, size, aspectRatio) {
